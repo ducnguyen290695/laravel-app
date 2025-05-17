06878e1628e6b61a5e0d62ca86420012071ef6d0
duc nguyen <ducnguyen@javis.vn>
2025-05-17 17:08:15 +0700

    versioning api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('users', UserController::class);
-
-
+Route::prefix('v1')->group(function () {
+    require __DIR__ . '/v1/users.php';
+});
