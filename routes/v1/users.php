23e30df7837e25ecdbaa432465a6740f1fc6ec67
duc nguyen <ducnguyen@javis.vn>
2025-05-18 11:58:27 +0700

    fix routing

--- routes/v1/users.php
+++ routes/v1/users.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('users')->group(function () {
-    Route::apiResource('/', UserController::class);
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show'])->whereNumber('id');
+    Route::put('/{id}', [UserController::class, 'update'])->whereNumber('id');
+    Route::delete('/{id}', [UserController::class, 'destroy'])->whereNumber('id');
 });
 
 
